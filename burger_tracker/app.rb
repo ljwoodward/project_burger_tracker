6f30f8032c399692c4cdc6f78280269b7aa69263
Laurence <ljwoodward.code@gmail.com>
2017-10-23 11:30:05 +0100

    created list of options in index

--- burger_tracker/app.rb
+++ burger_tracker/app.rb
@@ -1,5 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+require_relative('controllers/burgers_controller')
+require_relative('controllers/deals_controller')
+require_relative('controllers/eateries_controller')
 
 get '/' do
   erb( :index )
