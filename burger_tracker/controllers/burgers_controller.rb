bff5e48a0779196725e05fc947efe198139dd73a
Laurence <ljwoodward.code@gmail.com>
2017-10-23 11:37:01 +0100

    added show burger route

--- burger_tracker/controllers/burgers_controller.rb
+++ burger_tracker/controllers/burgers_controller.rb
@@ -4,5 +4,10 @@ require_relative( '../models/burger.rb' )
 
 get '/burgers' do
   @burgers = Burger.all
-  erb ( :"burgers/index" )
+  erb (:"burgers/index")
+end
+
+get '/burgers/:id' do
+  @burger = Burger.find(params[:id].to_i)
+  erb(:"burgers/show")
 end
