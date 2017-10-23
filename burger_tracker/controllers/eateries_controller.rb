cb7bf2df2e153aa61ada7d5ed32e315b9cfa0874
Laurence <ljwoodward.code@gmail.com>
2017-10-23 14:44:18 +0100

    added burgers to eateries/show view

--- burger_tracker/controllers/eateries_controller.rb
+++ burger_tracker/controllers/eateries_controller.rb
@@ -1,6 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/eatery.rb' )
+require_relative( '../models/burger.rb' )
+require_relative( '../models/deal.rb' )
 
 get '/eateries' do
   @eateries = Eatery.all
@@ -9,5 +11,6 @@ end
 
 get '/eateries/:id' do
   @eatery = Eatery.find(params[:id].to_i)
+  @burgers = @eatery.burgers
   erb(:"eateries/show")
 end
