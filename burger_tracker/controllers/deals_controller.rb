ddd7cc0066bf978384ca9149a057e4374632d855
Laurence <ljwoodward.code@gmail.com>
2017-10-24 09:42:42 +0100

    added post new route to deals_controller and form

--- burger_tracker/controllers/deals_controller.rb
+++ burger_tracker/controllers/deals_controller.rb
@@ -1,10 +1,24 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/deal.rb' )
+require_relative( '../models/eatery.rb' )
+require_relative( '../models/burger.rb' )
 
 get '/deals' do
   @deals = Deal.all
   erb (:"deals/index")
+end
+
+get '/deals/new' do
+  @eateries = Eatery.all
+  @burgers = Burger.all
+  erb(:"deals/new")
+end
+
+post '/deals' do
+  @deal = Deal.new(params)
+  @deal.save()
+  redirect to '/deals'
 end
 
 get '/deals/:id' do
